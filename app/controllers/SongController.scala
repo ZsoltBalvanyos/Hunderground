558db5ac83d82a3c63374c27c65dc731c77e1746
Zsolt Balvanyos <zsolt.balvanyos@ovoenergy.com>
2018-03-11 22:10:31 +0000

    Refactored song controller

--- app/controllers/SongController.scala
+++ app/controllers/SongController.scala
@@ -7,8 +7,7 @@ import play.api.cache.SyncCacheApi
 import play.api.mvc.ControllerComponents
 import repositories.SongRepository
 
-import scala.concurrent.Await
-import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class SongController @Inject()(cc: ControllerComponents,
                                songRepository: SongRepository,
@@ -16,29 +15,27 @@ class SongController @Inject()(cc: ControllerComponents,
                                cache: SyncCacheApi)
   extends SecuredController(cc, cache) {
 
-  def getSongs = SecuredAction { implicit request =>
+  def getSongs = SecuredAction.async { implicit request =>
     implicit val user = request.user
 
-    val allSongs = Await.result(songRepository.list(), 10.seconds)
-    val groupedSongs = allSongs.groupBy(_.status)
-    val songs = groupedSongs.get("song").getOrElse(List[Song]())
-    val backlogs = groupedSongs.get("backlog").getOrElse(List[Song]())
-
-    Ok(views.html.song(songs, backlogs))
+    songRepository
+      .list()
+      .map(songs => songs.groupBy(_.status))
+      .map(groupedSongs => {
+        val songs = groupedSongs.get("song").getOrElse(List[Song]())
+        val backlogs = groupedSongs.get("backlog").getOrElse(List[Song]())
+        Ok(views.html.song(songs, backlogs))
+      })
   }
 
   def addSong(artist: String, title: String, key: String, status: String) = SecuredAction { implicit request =>
     implicit val user = request.user
-
-    val song = Await.result(
-      songRepository.create(artist, title, status, key), 10.seconds
-    )
+    songRepository.create(artist, title, status, key)
     Ok
   }
 
   def deleteSong(songId: String) = SecuredAction { implicit request =>
     implicit val user = request.user
-
     songRepository.delete(songId)
     Ok
   }
